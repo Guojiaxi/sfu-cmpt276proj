2f81ae2e753a69338cef0478bcc5a7d9de5b08c9
BkCece <BkCece@users.noreply.github.com>
2020-07-04 19:12:48 -0700

    login page html and updates to index.js and css

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//Pool connection string
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+    connectionString: process.env.DATABASE_URL || 'postgres://postgres:cmpt276@localhost/study_scapes'
+})
+
+//Express 
+var app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//Set paths
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+//Get(s)
+app.get('/', (req, res) => res.render('pages/login'))
+
+//Listening - port
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
