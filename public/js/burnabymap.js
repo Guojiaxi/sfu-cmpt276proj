28fcaf49121b1d09dbf0499ae745c4f121e973f1
Pvakil <vakilparth98@gmail.com>
2020-07-07 15:16:56 -0700

    Add maps page for current location

--- public/js/burnabymap.js
+++ public/js/burnabymap.js
@@ -1,23 +1,85 @@
-
-window.onload = function() {
-    var script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCHOG78YkbxyjS10mfcWE0ivR_QIOjuIcw&callback=initMap';
-    script.defer = true;
-    script.async = true;
-    document.body.appendChild(script);
+/**
+ * Create google maps Map instance.
+ * @param {number} lat
+ * @param {number} lng
+ * @return {Object}
+ */
+const createMap = ({ lat, lng }) => {
+  return new google.maps.Map(document.getElementById('map'), {
+    center: { lat, lng },
+    zoom: 15
+  });
 };
 
-window.initMap = function() {
-
-    var mapOptions = {
-        zoom: 20,
-        minZoom: 15,
-        maxZoom: 20,
-        center: {lat: 49.277717, lng: -122.917871},
-        mapTypeControlOptions: { mapTypeIds: [] },
-        mapTypeId: 'roadmap',
-        streetViewControl: false
-    };
-
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+/**
+ * Create google maps Marker instance.
+ * @param {Object} map
+ * @param {Object} position
+ * @return {Object}
+ */
+const createMarker = ({ map, position, label }) => {
+  return new google.maps.Marker({ map, position, label });
 };
+
+/**
+ * Track the user location.
+ * @param {Object} onSuccess
+ * @param {Object} [onError]
+ * @return {number}
+ */
+const trackLocation = ({ onSuccess, onError = () => { } }) => {
+  if ('geolocation' in navigator === false) {
+    return onError(new Error('Geolocation is not supported by your browser.'));
+  }
+
+
+  return navigator.geolocation.watchPosition(onSuccess, onError, {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  });
+};
+
+/**
+ * Get position error message from the given error code.
+ * @param {number} code
+ * @return {String}
+ */
+const getPositionErrorMessage = code => {
+  switch (code) {
+    case 1:
+      return 'Permission denied.';
+    case 2:
+      return 'Position unavailable.';
+    case 3:
+      return 'Timeout reached.';
+  }
+}
+
+/**
+ * Initialize the application.
+ * Automatically called by the google maps API once it's loaded.
+*/
+function init() {
+  const initialPosition = { lat: 49.278136, lng: -122.920469 };
+  const map = createMap(initialPosition);
+  const marker = createMarker({ map, position: initialPosition, label: "You are here." });
+  const $info = document.getElementById('info');
+
+  let watchId = trackLocation({
+    onSuccess: ({ coords: { latitude: lat, longitude: lng } }) => {
+      marker.setPosition({ lat, lng });
+      console.log("watching")
+      console.log(lat)
+      console.log(lng)
+      map.panTo({ lat, lng });
+      $info.textContent = `Lat: ${lat.toFixed(10)} Lng: ${lng.toFixed(10)}`;
+      $info.classList.remove('error');
+    },
+    onError: err => {
+      console.log($info);
+      $info.textContent = `Error: ${err.message || getPositionErrorMessage(err.code)}`;
+      $info.classList.add('error');
+    }
+  });
+}
