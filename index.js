145a96ebcb4478e5a76d8077d6ce54e7c6e174db
BkCece <BkCece@users.noreply.github.com>
2020-07-09 05:27:39 -0700

    rebuilt login to use session and cookies, moved routes to new file, reorganized tabs, installed new packages

--- index.js
+++ index.js
@@ -1,112 +1,54 @@
-const express = require('express')
-const path = require('path')
+/*
+const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 5000
+*/
 
-//Pool connection string
-const { Pool } = require('pg');
+/**
+ * For the login session 
+ * https://medium.com/@timtamimi/getting-started-with-authentication-in-node-js-with-passport-and-postgresql-2219664b568c
+ */
+var express = require('express');
+require('dotenv').config();
+const PORT = process.env.PORT || 'postgres://postgres:cmpt276@localhost/study_scapes'
 
-/* Not sure what this is, but it breaks things?? */
-//const { REFUSED } = require('dns');
+var flash = require('connect-flash'); //middleware to notify user
 
-var pool;
-pool = new Pool({
-    connectionString: process.env.DATABASE_URL ||
-        /* change this according to your local postgres password */
-        'postgres://postgres:cmpt276@localhost/study_scapes'
-        //'postgres://postgres:root@localhost/study_scapes'
-})
+var passport = require("passport"); //authentication middleware - modular
+var request = require('request'); //for HTTP calls
 
-//Express
+/**
+ * More for login session
+ */
+
+var session = require('express-session'); //session management - cookies
+//app.use(expressSession({ secret: 'mySecretKey' }));
+
 var app = express();
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 
-//Set paths
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
 
-//Get for main page - login
-app.get('/', (req, res) => res.render('pages/login'))
-app.get('/login', (req, res) => res.render('pages/login'))
-app.get('/sign_up', (req, res) => res.render('pages/sign_up'))
-app.get('/about_app', (req, res) => res.render('pages/about_app'))
-app.get('/about_team', (req, res) => res.render('pages/about_team'))
-app.get('/burnaby/map', (req, res) => res.render('pages/burnaby/burnabymap'))
-app.get('/burnaby/events')
-app.get('/dashboard/:user_id/profile')
-app.get('/dashboard/:user_id/schedule')
-app.get('/dashboard/:user_id/schedule/create')
+//This order is ESSENTIAL: require, use, init(), sess()
+//https://stackoverflow.com/questions/29111571/passports-req-isauthenticated-always-returning-false-even-when-i-hardcode-done
+app.use(session({ secret: 'keyboard cat' }))
+app.use(passport.initialize());
+app.use(passport.session());
 
-// for admin only
-app.get('/admin/database')
+var bodyParser = require('body-parser') //for parsing json
 
-//Post for login & password
-//Will send the user to the appropriate home page (faculty or student)
-app.post('/login', (req, res) => {
-    uname = req.body.username;
-    pass = req.body.password;
+var path = require('path');
 
-    //checks for an existing username in the database with corresponding password
-    var unameQuery = 'SELECT * FROM login WHERE uname= \'' + uname + '\' AND password= \'' + pass + '\'';
+app.use('/public', express.static(__dirname + '/public'));
 
-    //Set return values for success and failure
-    pool.query(unameQuery, (error, result) => {
-        if (error) {
-            //username doesn't exist or pass is wrong
-            //reload login page
-            res.render('pages/login');
-        } else {
-            //user does exist
-            if (result.rows[0] == null) {
-                //incorrect password
-                res.render('pages/login');
-            } else {
-                //correct password
-                //this is pretty excessive, because string compare is being weird
-                var results = { 'results': result.rows }
-                var role = String(result.rows[0].role);
-                var faculty = "faculty";
-                var isFaculty = true;
+app.use(flash());
 
-                for (var i = 0; isFaculty == true && i < faculty.length; i++) {
-                    if (role[i] != faculty[i]) {
-                        isFaculty = false;
-                    }
-                }
+app.use(bodyParser());
+//app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
 
-                if (isFaculty) {
-                    res.render('pages/home_faculty', results);
-                } else {
-                    res.render('pages/home_student', results);
-                }
-            }
-        }
-    });
-})
+require('./lib/routes.js')(app); //might need this to contain routes and logic
 
-//Post for account creation
-//Will send user back to login page after sign up
-app.post('/sign_up', (req, res) => {
-    uname = req.body.username;
-    pass = req.body.password;
-    role = req.body.role;
-
-    //Add new row to table
-    var signupQuery = 'INSERT INTO login (uname, password, role) VALUES (\'' + uname + '\', \'' + pass + '\', \'' + role + '\')';
-    pool.query(signupQuery, (error, result) => {
-        if (error) {
-            res.redirect('/sign_up');
-        } else {
-            res.redirect('/login');
-        }
-    });
-
-})
-
-//Get for home pages
-//app.get('/home_st', (req, res) => res.render('pages/home_student'))
-//app.get('/home_fa', (req, res) => res.render('pages/home_faculty'))
-
-//Listening - port
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT);
+console.log('Node listening on port %s', PORT);
