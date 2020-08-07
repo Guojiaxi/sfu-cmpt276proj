6748cb3c2f7e253a432b8d1f88e71748c9add652
Pvakil <vakilparth98@gmail.com>
2020-08-06 21:48:29 -0700

    fix parth-test.js

--- test/parth-test.js
+++ test/parth-test.js
@@ -1,17 +1,17 @@
-let chai = require(‘chai’);
-let chaiHttp = require(‘chai-http’);
+let chai = require(chai');
+let chaiHttp = require('chai-http');
 var should = chai.should();
 chai.use(chaiHttp);
-let server = require(‘../lib/routes’);
+let server = require('../lib/routes');
 //Our parent block
-describe(‘Homepage’, () => {
- describe(‘/GET /’, () => {
-     it(‘it should GET all of the homepage’, (done) => {
+describe('Homepage', () => {
+ describe('/GET /', () => {
+     it('it should GET all of the homepage', (done) => {
      chai.request(server)
-       .get(‘/’)
+       .get('/')
        .end((err, res) => {
              (res).should.have.status(200);
-             (res.body).should.be.a(‘object’);
+             (res.body).should.be.a('object');
              (res.body.podcasts.length).should.be.eql(1);
              done();
           });
