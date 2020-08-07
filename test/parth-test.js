2f59d11f90aec4c58ba0980b237cf9bc5d651e7b
Pvakil <vakilparth98@gmail.com>
2020-08-06 21:32:25 -0700

    add parth-test.js

---
+++ test/parth-test.js
@@ -0,0 +1,19 @@
+let chai = require(‘chai’);
+let chaiHttp = require(‘chai-http’);
+var should = chai.should();
+chai.use(chaiHttp);
+let server = require(‘../lib/routes’);
+//Our parent block
+describe(‘Homepage’, () => {
+ describe(‘/GET /’, () => {
+     it(‘it should GET all of the homepage’, (done) => {
+     chai.request(server)
+       .get(‘/’)
+       .end((err, res) => {
+             (res).should.have.status(200);
+             (res.body).should.be.a(‘object’);
+             (res.body.podcasts.length).should.be.eql(1);
+             done();
+          });
+       });
+  });
